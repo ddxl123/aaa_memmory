a4e07c05bf4abc495cf0f10abd776939574de45a
linlong <1033839760@qq.com>
2024-01-23 08:12:52 +0800

    15

---
+++ subpackages/drift_main/lib/drift/table/entity/info/MemoryGroupSmartCycleInfos.dart
@@ -0,0 +1,36 @@
+
+part of drift_db;
+
+@ReferenceTo([])
+class MemoryGroupSmartCycleInfos extends CloudTableBase  {
+  @override
+  String? get tableName => "memory_group_smart_cycle_infos";
+  
+  @override
+  Set<Column>? get primaryKey => {id};
+
+  @ReferenceTo([Users])
+  IntColumn get creator_user_id => integer().named("creator_user_id")();
+
+  TextColumn get loop_cycle => text().named("loop_cycle")();
+
+  @ReferenceTo([MemoryAlgorithms])
+  IntColumn get memory_algorithm_id => integer().named("memory_algorithm_id")();
+
+  @ReferenceTo([MemoryGroups])
+  IntColumn get memory_group_id => integer().named("memory_group_id")();
+
+  IntColumn get small_cycle_order => integer().named("small_cycle_order")();
+
+  IntColumn get small_cycle_should_new_learn_count => integer().named("small_cycle_should_new_learn_count")();
+
+  IntColumn get small_cycle_should_review_count => integer().named("small_cycle_should_review_count")();
+
+  DateTimeColumn get created_at => dateTime().named("created_at")();
+
+  IntColumn get id => integer().named("id")();
+
+  DateTimeColumn get updated_at => dateTime().named("updated_at")();
+
+}
+        
